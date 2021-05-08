14caad0e7d0c14b4b29adb81711ed5d45b986540
JuaresSipert <juaressipert@gmail.com>
2021-05-07 23:22:12 -0300

    Incluindo validação

--- AspnetCore_WebApi/Models/Cliente.cs
+++ AspnetCore_WebApi/Models/Cliente.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AspnetCore_WebApi.Models
 {
     public class Cliente
     {
         public long Id { get; set; }
+        [Required(ErrorMessage ="Nome é Obrigatório")]
         public string Nome { get; set; }
         public DateTime DtNascimento { get; set; }
         public string NomeMae { get; set;}
