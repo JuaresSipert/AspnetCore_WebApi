740e022b2142ab3fbcd0b5975d4a75d10c07a3ec
JuaresSipert <juaressipert@gmail.com>
2021-05-07 22:27:37 -0300

    adicionado nome da mãe

--- AspnetCore_WebApi/Models/Cliente.cs
+++ AspnetCore_WebApi/Models/Cliente.cs
@@ -10,6 +10,7 @@ namespace AspnetCore_WebApi.Models
         public long Id { get; set; }
         public string Nome { get; set; }
         public DateTime DtNascimento { get; set; }
+        public string NomeMae { get; set;}
  
 
     }
